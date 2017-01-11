8a5ec28e2e835e75b822a18af63e98f9f2fabb21
Test <test@Tests-MacBook-Pro.local>
2017-01-10 20:02:23 -0800

    added links to about, contact etc..

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+class AboutController < ApplicationController
+	def index
+		get 'index.html'
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 root 'welcome#homepage'
-#get 'welcome/homepage'
+get '/index', to: 'welcome#index'
+get '/about', to: 'about#about'
+get '/contact', to: 'contact#contact'
+get '/blog', to: 'blog#blog'
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
